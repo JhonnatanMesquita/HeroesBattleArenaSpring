d9da67a15347fa6f158ad70569b0c8e21517feba
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2020-02-24 01:38:04 -0300

    Corrigindo a disponibilidade de alguns end-points sem autenticação

--- src/main/java/me/jhonnatanmesquita/heroesbattlearena/security/configs/SecurityConfig.java
+++ src/main/java/me/jhonnatanmesquita/heroesbattlearena/security/configs/SecurityConfig.java
@@ -39,13 +39,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private JWTUtil jwtUtil;
 
     private static final String[] PUBLIC_MATCHERS_GET = {
-            "/login",
             "/jogador/**",
             "/heroi/**"
     };
 
     private static final String[] PUBLIC_MATCHERS_POST ={
-            "/**"
+            "/login",
+            "/jogador"
     };
 
     @Override
